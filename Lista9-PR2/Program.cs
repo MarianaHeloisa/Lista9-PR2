15e080e94f1e71d9e403e7bcbd6f442923b43903
MarianaHeloisa <mariana.heloisa@aluno.ifsp.edu.br>
2023-05-31 12:56:12 -0300

    Adicionar arquivos de projeto.

---
+++ Lista9-PR2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista9_PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] vet2;
+
+            //Console.WriteLine("entre com o tamanho ");
+            //int t = int.Parse(Console.ReadLine());
+
+            //vet2 = new int[t];
+            //Random rand = new Random();
+
+            //float soma = 0;
+            //for (int i = 0; i < t; i++) 
+            //{
+            //    vet2[i] = rand.Next(10, 50);
+            //    soma = soma + vet2[i];
+
+            //}
+
+            //Console.WriteLine("a media é = " + soma/ (float) t );
+            
+            //for (int i = 0; i < t; i++)
+            //{
+            //    Console.WriteLine("posicao [{0}] = {1} ", vet2[i]);
+
+                
+                // criar um programa que mostre a soma dos numeros pares e
+                //soma dos numeros impares contidos no vetor. Considere o 
+                //exercicio anterior para gerar o vetor.
+
+            }
+        }
+    }
+}
